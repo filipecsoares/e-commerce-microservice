dae3c02995ff96b35a0d474511a195eecbfe5d60
filipecsoares <filipecarlossoares@gmail.com>
2024-08-17 07:54:46 -0300

    update customer

--- services/customer/src/main/java/com/fcs/ecommerce/customer/CustomerController.java
+++ services/customer/src/main/java/com/fcs/ecommerce/customer/CustomerController.java
@@ -2,11 +2,9 @@ package com.fcs.ecommerce.customer;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -17,6 +15,12 @@ public class CustomerController {
 
     @PostMapping
     public ResponseEntity<String> createCustomer(@RequestBody @Valid CustomerRequest request) {
-        return ResponseEntity.ok(this.service.createCustomer(request));
+        return new ResponseEntity<>(this.service.createCustomer(request), HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateCustomer(@RequestBody @Valid CustomerRequest request) {
+        this.service.updateCustomer(request);
+        return ResponseEntity.accepted().build();
     }
 }
--- services/customer/src/main/java/com/fcs/ecommerce/customer/CustomerService.java
+++ services/customer/src/main/java/com/fcs/ecommerce/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.fcs.ecommerce.customer;
 
+import com.fcs.ecommerce.exception.CustomerNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +15,26 @@ public class CustomerService {
     public String createCustomer(CustomerRequest request) {
         var customer = this.repository.save(mapper.toCustomer(request));
         return customer.getId();
+    }
+
+    public void updateCustomer(CustomerRequest request) {
+        var customer = this.repository.findById(request.id())
+                .orElseThrow(() -> new CustomerNotFoundException(
+                        String.format("Cannot update customer: No customer found with the provided ID: %s", request.id())
+                ));
+        mergeCustomer(customer, request);
+        this.repository.save(customer);
+    }
+
+    private void mergeCustomer(Customer customer, CustomerRequest request) {
+        if (StringUtils.isNotBlank(request.firstname())) {
+            customer.setFirstname(request.firstname());
+        }
+        if (StringUtils.isNotBlank(request.email())) {
+            customer.setEmail(request.email());
+        }
+        if (request.address() != null) {
+            customer.setAddress(request.address());
+        }
     }
 }
